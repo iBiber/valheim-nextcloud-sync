40aa8e6bbabf6268c8477f12411ac207f98fe598
iBiber <iBiber@outlook.de>
2023-08-26 14:45:40 +0000

    Use always the map name in the path; Improve logging

--- download_map.sh
+++ download_map.sh
@@ -5,11 +5,14 @@ echo "Nextcloud URL: $NEXTCLOUD_URL/$NEXTCLOUD_MODE$NEXTCLOUD_PATH"
 echo "Nextcloud user: $NEXTCLOUD_USER"
 echo "Map name: $WORLD_NAME"
 
-echo "Get latest file name from: $NEXTCLOUD_URL/$NEXTCLOUD_MODE$NEXTCLOUD_PATH/latest_file.txt"
-export datetimestamp=$(curl -u $NEXTCLOUD_USER:$NEXTCLOUD_PASSWORD $NEXTCLOUD_URL/$NEXTCLOUD_MODE$NEXTCLOUD_PATH/latest_file.txt)
+baseUrl="$NEXTCLOUD_USER:$NEXTCLOUD_PASSWORD $NEXTCLOUD_URL/$NEXTCLOUD_MODE$NEXTCLOUD_PATH/$WORLD_NAME"
+url="$baseUrl/latest_file.txt"
+echo "Get latest file name from: $url"
+export datetimestamp=$(curl -u $url)
 
-echo "Download map archive from: $NEXTCLOUD_URL/$NEXTCLOUD_MODE$NEXTCLOUD_PATH/$datetimestamp.tar.gz"
-curl -u $NEXTCLOUD_USER:$NEXTCLOUD_PASSWORD $NEXTCLOUD_URL/$NEXTCLOUD_MODE$NEXTCLOUD_PATH/$datetimestamp.tar.gz > $WORLD_NAME.tar.gz
+url="$baseUrl/$datetimestamp.tar.gz"
+echo "Download map archive from: $url"
+curl -u $NEXTCLOUD_USER:$NEXTCLOUD_PASSWORD $url > $WORLD_NAME.tar.gz
 
 echo "Extract map archive to valheim-server"
 tar -v --touch --overwrite --extract --gunzip --file=$WORLD_NAME.tar.gz
--- upload_map.sh
+++ upload_map.sh
@@ -6,16 +6,20 @@ echo "Nextcloud user: $NEXTCLOUD_USER"
 echo "Map name: $WORLD_NAME"
 
 # local variables
-export datetimestamp=$(date '+%d-%b-%YT%H%M')
+export datetimestamp=$(date '+%Y-%m-%dT%H%M')
 
 echo "Create tar.gz of map $WORLD_NAME"
 tar -v --create --gzip --file=$WORLD_NAME.$datetimestamp.tar.gz /config/worlds_local/$WORLD_NAME.db /config/worlds_local/$WORLD_NAME.fwl /config/valheimplus/valheim_plus.cfg
 
-echo "Upload map archive to $NEXTCLOUD_URL/$NEXTCLOUD_MODE$NEXTCLOUD_PATH/$datetimestamp.tar.gz"
-curl -u $NEXTCLOUD_USER:$NEXTCLOUD_PASSWORD -T $WORLD_NAME.$datetimestamp.tar.gz $NEXTCLOUD_URL/$NEXTCLOUD_MODE$NEXTCLOUD_PATH/$datetimestamp.tar.gz
+baseUrl=$NEXTCLOUD_URL/$NEXTCLOUD_MODE$NEXTCLOUD_PATH/$WORLD_NAME
 
-echo "Update latest_file.txt with $datetimestamp to $NEXTCLOUD_URL/$NEXTCLOUD_MODE$NEXTCLOUD_PATH/latest_file.txt"
-echo $datetimestamp | curl -u $NEXTCLOUD_USER:$NEXTCLOUD_PASSWORD -T - $NEXTCLOUD_URL/$NEXTCLOUD_MODE$NEXTCLOUD_PATH/latest_file.txt
+url="$baseUrl/$datetimestamp.tar.gz"
+echo "Upload map archive to $url"
+curl -u $NEXTCLOUD_USER:$NEXTCLOUD_PASSWORD -T $WORLD_NAME.$datetimestamp.tar.gz $url
+
+url="$baseUrl/latest_file.txt"
+echo "Update latest_file.txt with $datetimestamp to $url"
+echo $datetimestamp | curl -u $NEXTCLOUD_USER:$NEXTCLOUD_PASSWORD -T - $url
 
 echo "Clean up"
 rm $WORLD_NAME.$datetimestamp.tar.gz
